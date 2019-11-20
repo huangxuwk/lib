4ebc0e0f346715a0566b8a7df1cd667c746d9679
hx-climb <56286527+hx-climb@users.noreply.github.com>
2019-11-20 17:33:58 +0800

    Add files via upload

---
+++ 20191120@多文件云传输（3）/test/Test.java
@@ -0,0 +1,21 @@
+package com.dl.mmfct.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+//		Resource rs = new Resource()
+//				.setAppName("1")
+//				.setId("1")
+//				.setVersion("1")
+//				.setAbsoluteRoot("N:\\\\¾­µä");
+//		rs.exploreResource();
+//		System.out.println(rs);
+		int size = 0;
+		size |= 0x2;
+		size |= 0x4;
+		
+		System.out.println(size);
+	
+	}
+
+}
