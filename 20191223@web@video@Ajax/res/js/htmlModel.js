5b5f9604185a2c0315b19c36120b6392ee89589c
hx-climb <56286527+hx-climb@users.noreply.github.com>
2019-12-23 19:53:42 +0800

    Add files via upload

---
+++ 20191223@web@video@Ajax/res/js/htmlModel.js
@@ -0,0 +1,61 @@
+(function () {
+	$$.htmlModel = {
+		normalNav : 
+		'<nav class="navbar navbar-default" role="navigation">'
+		    + '<div class="container-fluid">'
+			    + '<div class="navbar-header">'
+			        + '<a class="navbar-brand" href="{firstHref=#index.html}">{firstItem=First}</a>'
+			    + '</div>'
+			    + '<div>'
+			        + '<ul class="nav navbar-nav">'
+			            + 'for(liList){<li class="{itemActive=active}">'
+			            	+ '<a id="{itemId=}" href="{itemHref=}">'
+			            	+ '{itemContext=Item}</a>'
+			            + '</li>}liList'
+			        + '</ul>'
+			    + '</div>'
+		    + '</div>'
+		+ '</nav>',
+		blankModal :
+		'<div class="modal fade" id="{modalId=}" tabindex="-1" role="dialog" aria-labelledby="modalTopic" aria-hidden="true">'
+			+ '<div class="modal-dialog {modal_style=}">'
+				+ '<div class="modal-content">'
+					+ '<div class="modal-header">'
+						+ '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>'
+						+ '<h4 class="modal-title {modalTopicStyle=}" id="modalTopic">{modalTopic=模态框标题}</h4>'
+					+ '</div>'
+					+ '<div id="{modal_body_id=}" class="modal-body"></div>'
+					+ '<div class="modal-footer">'
+					+ '<button id="{btnCloseId=}" type="button" class="btn btn-default" data-dismiss="modal">关闭</button>'
+						+ '<button id="{btnSubmit=}" type="button" class="btn btn-primary">提交更改</button>'
+					+ '</div>'
+				+ '</div>'
+			+ '</div>'
+		+ '</div>',
+		normalInputGroup :
+		'for(normalInputGroup){<div class="input-group">'
+			+ '<span class="input-group-addon {inputStyle=}">{inputLabel=}</span>'
+			+ '<input id="{inputId=}" type="{inputType=text}" class="form-control" placeholder="{inputPlaceholder=}">'
+		+ '</div>}normalInputGroup',
+		normalCarousel :
+		'<div id="{carouselId=}" data-ride="carousel" data-interval="{intervalTime=5000}" class="carousel slide {carouselStyle=}">'
+			+ '<ol class="carousel-indicators">'
+				+ 'for(carourselIndex){<li data-target="#{carouselId=}" data-slide-to="{index=0}" class="{indexActive=active}"></li>}carourselIndex'
+		    + '</ol>   '
+			+ '<div class="carousel-inner">'
+				+ 'for(carouselItem){<div class="item {itemActive=active}">'
+					+ '<img class="{itemStyle=}" src="{imgPath=}" alt="{imgAlt=}">'
+					+ '<div class="carousel-caption">{itemCaption=}</div>'
+				+ '</div>}carouselItem'
+			+ '</div>'
+			+ '<a class="left carousel-control" href="#{carouselId=}" role="button" data-slide="prev">'
+				+ '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>'
+				+ '<span class="sr-only">Previous</span>'
+			+ '</a>'
+			+ '<a class="right carousel-control" href="#{carouselId=}" role="button" data-slide="next">'
+				+ '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'
+				+ '<span class="sr-only">Next</span>'
+			+ '</a>'
+		+ '</div>',
+	};
+})();
