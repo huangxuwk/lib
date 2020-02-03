abd623d0cd20667998291937be2278e38f94b419
hx-climb <56286527+hx-climb@users.noreply.github.com>
2020-02-03 23:33:19 +0800

    Add files via upload

---
+++ 20200203@LeetCode@MaxSubArray/MaxSubArray.java
@@ -0,0 +1,20 @@
+package com.dl.test2;
+
+public class MaxSubArray {
+
+	public MaxSubArray() {
+	}
+	
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0)
+                sum += num;
+            else
+                sum = num;
+            res = Math.max(res, sum);
+        }
+        return res;     
+    }
+}
