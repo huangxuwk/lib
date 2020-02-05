a67892eeae756383063f1f36a1d3a7c799f49614
hx-climb <56286527+hx-climb@users.noreply.github.com>
2020-02-05 23:33:48 +0800

    Add files via upload

---
+++ 20200205@LeetCode@LengthOfLastWord/LengthOfLastWord.java
@@ -0,0 +1,19 @@
+package com.dl.test3;
+
+public class LengthOfLastWord {
+
+	public LengthOfLastWord() {
+	}
+	
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else if (length != 0) {
+                return length;
+            }
+        }
+        return length;        
+    }
+}
