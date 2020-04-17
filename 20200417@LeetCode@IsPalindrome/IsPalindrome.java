39dac9234124198c209c64819854efbc011ba63c
huangxuwk <56286527+huangxuwk@users.noreply.github.com>
2020-04-17 11:05:44 +0800

    Add files via upload

---
+++ 20200417@LeetCode@IsPalindrome/IsPalindrome.java
@@ -0,0 +1,30 @@
+package com.dl.test4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dl.util.ListNode;
+
+public class IsPalindrome {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int start = 0;
+        int end = list.size() - 1;
+        while (start < end) {
+            if (!list.get(start).equals(list.get(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
